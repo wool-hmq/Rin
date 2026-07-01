147c6fc86799f5796705b12008898dd40d3fb18c
wool-hmq <woolhmq@126.com>
2026-07-01 08:16:12 +0800

    Create r2.ts

---
+++ client/src/config/r2.ts
@@ -0,0 +1,7 @@
+export const R2_BASE = 'https://r2page.jiaoblog.dpdns.org'; // 替换为你的实际域名
+
+export const R2_ENDPOINTS = {
+  MENU: `${R2_BASE}/menu.json`,
+  SPONSOR: `${R2_BASE}/sponsor.json`,
+  SIDEBAR: `${R2_BASE}/sidebar.json`, 
+};
